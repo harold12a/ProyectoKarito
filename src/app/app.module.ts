c33e59b16a299476af7190b90ae5a92ff3b7b6e5
harold alzate <haroldalzate99@gmail.com>
2022-11-17 16:39:58 -0500

    firt commit arming  index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ProductosComponent } from './productos/productos.component';
+import { SedesComponent } from './sedes/sedes.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ContabilidadComponent } from './contabilidad/contabilidad.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'Productos', component: ProductosComponent},
+  {path: 'Sedes', component: SedesComponent},
+  {path: 'Servicios', component: ServiciosComponent},
+  {path: 'Contacto', component: ContactoComponent},
+  {path: 'Contabilidad', component: ContabilidadComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServiciosComponent,
+    ProductosComponent,
+    SedesComponent,
+    ContactoComponent,
+    ContabilidadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
